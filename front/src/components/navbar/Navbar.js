3467d05054ddbfbf971c9f3af750e6bdfc86a671
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-14 14:28:27 -0400

    update

---
+++ front/src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+   <header>
+    <nav>
+        Header
+    </nav>
+   </header>
+  )
+}
+
+export default Navbar
